952e9085f5f782013000be3a43183e62d67f5ccb
Bradley Howell <bradley.howell48@ed.cna.nl.ca>
2025-12-03 19:19:51 -0330

    Added a function to create the deck of cards and code to test it in the main function.

--- blackjack.py
+++ blackjack.py
@@ -9,9 +9,27 @@ def write_money(money):
     with open("money.txt", "w") as file:
         file.write(str(money))
 
+def create_deck():
+    suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+    ranks = ["Ace", "King", "Queen", "Jack", "10",
+             "9", "8", "7", "6", "5", "4", "3", "2"]
+    deck = []
+    for suit in suits:
+        for rank in ranks:
+            if rank == "Ace":
+                value = None
+            elif rank in ["King", "Queen", "Jack"]:
+                value = 10
+            else:
+                value = int(rank)
+            card = [suit, rank, value]
+            deck.append(card)
+    return deck
+
 def main():
-    money = read_money()
-    write_money(money)
+    deck = create_deck()
+    for card in deck:
+        print(card)
     
 if __name__ == "__main__":
     main()
